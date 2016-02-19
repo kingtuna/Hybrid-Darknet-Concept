4fcd36bb02950625dcfb2d01cc42a79201b04cd3
Terrence Gareau <secretpackets@gmail.com>
2016-02-19 15:34:23 -0800

    Added Go based Emulators
    Potential security issues with c based emulators. Migrated to GO

--- hp-installer-active.sh
+++ hp-installer-active.sh
@@ -49,20 +49,20 @@ killall ntpd
 /usr/local/bin/ntpd -u ntp:ntp -p /var/run/ntpd.pid -g
 killall sshpot
 service sshpot restart
-killall SSDP_Emulator
-/root/build/ssdp/SSDP_Emulator
-killall heartbeat_emulator
-/root/build/heartbeat/heartbeat_emulator
-killall mdns_emulator
-/root/build/mdns/mdns_emulator
-killall nat-pmp_emulator
-/root/build/nat-pmp/nat-pmp_emulator
-killall quake_emulator
-/root/build/quake_emu/quake_emulator
-killall ripv1_emulator
-/root/build/ripv1/ripv1_emulator
-killall sentinel_emulator
-/root/build/sentinel/sentinel_emulator
+killall hp-ssdp
+/root/build/ssdp/hp-ssdp &
+killall hp-heartbeat
+/root/build/heartbeat/hp-heartbeat &
+killall hp-mdns
+/root/build/mdns/hp-mdns &
+killall hp-nat-pmp
+/root/build/nat-pmp/hp-nat-pmp &
+killall hp-quake
+/root/build/quake_emu/hp-quake &
+killall hp-ripv1
+/root/build/ripv1/hp-ripv1 &
+killall hp-sentinal
+/root/build/sentinel/hp-sentinal &
 /usr/sbin/hping3 --rand-source -c 600 --udp -p 123 --fast -n 127.0.0.1 -d 48 -E /root/ntp.bin' > /root/killntp.sh 
 chmod +x /root/killntp.sh 
 
@@ -100,57 +100,55 @@ service xinetd restart
 cd /root/build/
 mkdir ssdp
 cd /root/build/ssdp/
-curl https://raw.githubusercontent.com/kingtuna/honeynet/master/emulators/SSDP_Emulator.c > SSDP_Emulator.c
-gcc SSDP_Emulator.c -o SSDP_Emulator
-/root/build/ssdp/SSDP_Emulator
+wget https://github.com/kingtuna/go-emulators/releases/download/0.09/hp-ssdp
+chmod +x hp-ssdp
 
 ###### Install heartbeat_emulator #######
 if [ ! -d /root/build/heartbeat ]; then
-	mkdir /root/build/heartbeat
+    mkdir /root/build/heartbeat
 fi
-cd /root/build/heartbeat && curl https://raw.githubusercontent.com/kingtuna/Hybrid-Darknet-Concept/master/emulators/heartbeat_emulator.c > heartbeat_emulator.c
-gcc heartbeat_emulator.c -o heartbeat_emulator
-/root/build/heartbeat/heartbeat_emulator
+cd /root/build/heartbeat
+wget https://github.com/kingtuna/go-emulators/releases/download/0.09/hp-heartbeat
+chmod +x hp-heartbeat
 
 ###### Install mdns Service Emulator #######
 if [ ! -d /root/build/mdns ]; then
-	mkdir /root/build/mdns
+    mkdir /root/build/mdns
 fi
-cd /root/build/mdns && curl https://raw.githubusercontent.com/kingtuna/Hybrid-Darknet-Concept/master/emulators/mdns_emulator.c > mdns_emulator.c
-gcc mdns_emulator.c -o mdns_emulator
-/root/build/mdns/mdns_emulator
+cd /root/build/mdns 
+wget https://github.com/kingtuna/go-emulators/releases/download/0.09/hp-mdns
+chmod +x hp-mdns
 
 ##### Install nat-pmp Service Emulator #####
 if [ ! -d /root/build/nat-pmp ]; then
-	mkdir /root/build/nat-pmp
+    mkdir /root/build/nat-pmp
 fi
-cd /root/build/nat-pmp/ && curl https://raw.githubusercontent.com/kingtuna/Hybrid-Darknet-Concept/master/emulators/nat-pmp_emulator.c > nat-pmp_emulator.c
-gcc nat-pmp_emulator.c -o nat-pmp_emulator
-/root/build/nat-pmp/nat-pmp_emulator
+cd /root/build/nat-pmp/
+wget https://github.com/kingtuna/go-emulators/releases/download/0.09/hp-nat-pmp
+chmod +x hp-nat-pmp
 
 ##### Install quake Service Emulator *****
 if [ ! -d /root/build/quake_emu ]; then
-	mkdir /root/build/quake_emu
+    mkdir /root/build/quake_emu
 fi
-cd /root/build/quake_emu && curl https://raw.githubusercontent.com/kingtuna/Hybrid-Darknet-Concept/master/emulators/quake_emulator.c > quake_emulator.c
-gcc quake_emulator.c -o quake_emulator
-/root/build/quake_emu/quake_emulator
+cd /root/build/quake_emu
+wget https://github.com/kingtuna/go-emulators/releases/download/0.09/hp-quake
+chmod +x hp-quake
 
 ##### Install ripv1 Service Emulator *****
 if [ ! -d /root/build/ripv1 ]; then
-	mkdir /root/build/ripv1
+    mkdir /root/build/ripv1
 fi
-cd /root/build/ripv1 && curl https://raw.githubusercontent.com/kingtuna/Hybrid-Darknet-Concept/master/emulators/ripv1_emulator.c > ripv1_emulator.c
-gcc ripv1_emulator.c -o ripv1_emulator
-/root/build/ripv1/ripv1_emulator
+cd /root/build/ripv1
+wget https://github.com/kingtuna/go-emulators/releases/download/0.09/hp-ripv1
+chmod +x hp-ripv1
 
 ###### Install sentinel Service Emulator ######
 cd /root/build/
 mkdir sentinel
 cd /root/build/sentinel/
-curl https://raw.githubusercontent.com/kingtuna/honeynet/master/emulators/sentinel_emulator.c > sentinel_emulator.c
-gcc sentinel_emulator.c -o sentinel_emulator
-/root/build/sentinel/sentinel_emulator
+wget https://github.com/kingtuna/go-emulators/releases/download/0.09/hp-sentinal
+chmod +x hp-sentinal
 
 ###### Install Recursive DNS ######
 
@@ -445,12 +443,15 @@ export PATH=/nsm/bro/bin:$PATH
 /nsm/bro/bin/broctl install
 /nsm/bro/bin/broctl start
 sleep 2
-hping3 --rand-source -c 600 --udp -p 123 --fast -n 127.0.0.1 -d 48 -E /root/ntp.bin
 service logstash start
-killall sentinel_emulator
-killall SSDP_Emulator
-/root/build/ssdp/SSDP_Emulator
-/root/build/sentinel/sentinel_emulator
+/root/build/ssdp/hp-ssdp &
+/root/build/heartbeat/hp-heartbeat &
+/root/build/mdns/hp-mdns &
+/root/build/nat-pmp/hp-nat-pmp &
+/root/build/quake_emu/hp-quake &
+/root/build/ripv1/hp-ripv1 &
+/root/build/sentinel/hp-sentinal &
+hping3 --rand-source -c 600 --udp -p 123 --fast -n 127.0.0.1 -d 48 -E /root/ntp.bin
 
 exit 0
 ' > /etc/rc.local 
